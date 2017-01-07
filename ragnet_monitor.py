74109787f66e807d8886ced4e542ab707a8a5a1a
Tom Weston <weston.tom@gmail.com>
2017-01-07 14:44:50 +0000

    reversing colours

--- ragnet_monitor.py
+++ ragnet_monitor.py
@@ -54,11 +54,11 @@ while True:
     x = 0
     while x < 50:
         if dns:
-            #We have internet, play the happy lights.
-                set_pixel(7, 255, 0, 0)
-        else:
-            #DNS and/or internet services are down, play the angry lights
+            #We have internet, set LED to GREEN
                 set_pixel(7, 0, 255, 0)
+        else:
+            #DNS and/or internet services are down, set LED to RED
+                set_pixel((7, 255, 0, 0),(6, 255, 0, 0),(6, 255, 0, 0))
         show()
         x += 1
         time.sleep(0.10)
